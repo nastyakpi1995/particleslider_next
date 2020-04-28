af056643c7e62ec46faef03b7134ce5217f36c5a
Anastasia Viktorivna Krat <47394988+nastyakpi1995@users.noreply.github.com>
2020-04-28 18:12:20 +0300

    add particle slider library (#3)
    * add particle slider library
    
    * code split
    
    * add particlesSlider.json

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import Head from "next/head";
+import Head from 'next/head';
+import Particles from 'react-particles-js';
 
 import { GlobalStyle } from '../components/styled/homePage';
 import TopMenu from '../components/TopMenu/topMenu';
+import paramToObj from '../components/configs/particlesSlider';
 
 const HomePage = () => (
-   <div>
-       <Head>
-           <title>Axels</title>
-       </Head>
-       <GlobalStyle />
-       <TopMenu />
-   </div>
+    <div>
+        <Head>
+            <title>Axels</title>
+        </Head>
+        <GlobalStyle />
+        <TopMenu />
+        <Particles
+            params={paramToObj}
+        />
+    </div>
 );
 
 export default HomePage;
